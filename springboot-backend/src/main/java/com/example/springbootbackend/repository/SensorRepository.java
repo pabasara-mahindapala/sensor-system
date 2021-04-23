677399d070573093221fa567a3afd35a354f2804
Pasiexpress <pmjayasinghe1025@gmail.com>
2021-04-23 08:11:33 +0530

    add the repository file

---
+++ springboot-backend/src/main/java/com/example/springbootbackend/repository/SensorRepository.java
@@ -0,0 +1,11 @@
+package com.example.springbootbackend.repository;
+
+import com.example.springbootbackend.model.Sensor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SensorRepository extends JpaRepository<Sensor,Long> {
+    Sensor em = new Sensor();
+
+}
