aa3b7b5f5f6d9d8fa71091922b43b99a6d04c33f
Pasiexpress <pmjayasinghe1025@gmail.com>
2021-04-23 08:08:12 +0530

    add the controller file

---
+++ springboot-backend/src/main/java/com/example/springbootbackend/controller/SensorController.java
@@ -0,0 +1,28 @@
+package com.example.springbootbackend.controller;
+
+
+import com.example.springbootbackend.model.Sensor;
+import com.example.springbootbackend.repository.SensorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+@RequestMapping("/api/v1/")
+public class SensorController {
+
+    @Autowired
+    private SensorRepository sensorRepository;
+
+    // get all employees
+    @GetMapping("/sensors")
+    public List<Sensor> getAllEmployees(){
+        return sensorRepository.findAll();
+    }
+    // create employee rest api
+    @PostMapping("/sensors")
+    public Sensor createEmployee(@RequestBody Sensor sensor) {
+        return sensorRepository.save(sensor);
+    }
+}
