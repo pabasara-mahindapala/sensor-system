597aaa09a20c38748aea109f56be35082d542b87
pabasaramahindapala <pabasaramahindapala@gmail.com>
2021-04-25 09:34:31 +0530

    integrate with sensors backend

--- src/app/areas/sensors/sensors.component.ts
+++ src/app/areas/sensors/sensors.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from "@angular/common/http";
 import { Component } from "@angular/core";
 import { Sensor } from "./sensors.model";
 
@@ -7,66 +8,39 @@ import { Sensor } from "./sensors.model";
 	styleUrls: ["./sensors.component.scss"],
 })
 export class SensorsComponent {
-	sensors: Sensor[] = [
-		{
-			id: 1,
-			title: "Outdoor Temperature",
-			tag: "Online",
-			url: "https://github.com/sketch7/ssv-core",
-			description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-		},
-		{
-			id: 2,
-			title: "Boiler 1 Temp",
-			tag: "Error",
-			url: "https://github.com/sketch7/signalr-client",
-			description: "Vestibulum porttitor urna lectus, bibendum sodales augue mattis et",
-		},
+	sensorResponse: any;
+	sensors: Sensor[] = [];
 
-		{
-			id: 3,
-			title: "Boiler 2 Temp",
-			tag: "Online",
-			url: "https://github.com/sketch7/ngx.command",
-			description:
-				"Donec mi velit, rhoncus at dolor luctus, fermentum vehicula ex",
-		},
-		{
-			id: 4,
-			title: "Pipe Temperature",
-			tag: "Warning",
-			url: "https://github.com/sketch7/angular-skeleton-app",
-			description: "Vestibulum ultrices odio in mauris lobortis, eget posuere arcu malesuada",
-		},
-		// {
-		// 	key: "ng2-heroes",
-		// 	title: "ng2 heroes app",
-		// 	tag: "Warning",
-		// 	url: "https://github.com/sketch7/ng2-heroes",
-		// 	description: "angular2 heroes sample application",
-		// },
+	constructor(private http: HttpClient) {
+		this.getSensors();
+	}
 
-		// {
-		// 	key: "ssv-au-core",
-		// 	title: "@ssv/au-core",
-		// 	tag: "Online",
-		// 	url: "https://github.com/sketch7/ssv-au-core",
-		// 	description: "Core components, utilities and services for Aurelia by Sketch7",
-		// },
-		// {
-		// 	key: "ssv-au-ui",
-		// 	title: "@ssv/au-ui",
-		// 	tag: "Online",
-		// 	url: "https://github.com/sketch7/ssv-au-ui",
-		// 	description: "UI components library for Aurelia by Sketch7",
-		// },
+	getSensors() {
+		this.http.get("http://localhost:8080/api/v1/sensors").subscribe(result => {
+			this.sensorResponse = result;
+			this.sensorResponse.sort((a:any, b:any) => new Date(b.date).getTime() - new Date(a.date).getTime());
+			for (var s in this.sensorResponse) {				
+				if (!this.sensors.find(x => x.sensorID == this.sensorResponse[s].sensorID)) {
+					this.sensors.push({
+						id: this.sensorResponse[s].id,
+						sensorID: this.sensorResponse[s].sensorID,
+						date: this.sensorResponse[s].date,
+						dataValue: this.sensorResponse[s].dataValue,
+						status: this.sensorResponse[s].status
+					});
+				}
 
-		// {
-		// 	key: "fluently-http-client",
-		// 	title: "Fluently Http Client",
-		// 	tag: "Error",
-		// 	url: "https://github.com/sketch7/FluentlyHttpClient",
-		// 	description: "Http Client for .NET Standard with fluent APIs which are intuitive, easy to use and also highly extensible.",
-		// },
-	];
+			}
+		});
+		// this.sensors = [
+		// 	{
+		// 		id: 6,
+		// 		sensorID: "test",
+		// 		date: new Date(),
+		// 		dataValue: 60,
+		// 		status: "Online",
+		// 	}];
+
+		// console.log(this.http);
+	}
 }
--- src/app/areas/sensors/sensors.model.ts
+++ src/app/areas/sensors/sensors.model.ts
@@ -3,8 +3,8 @@ export type TagType = "Online" | "Error" | "Warning";
 
 export interface Sensor {
 	id: number;
-	title: string;
-	description?: string;
-	url: string;
-	tag?: TagType;
+	sensorID: string;
+	date: Date;
+	dataValue: number;
+	status?: TagType;
 }
