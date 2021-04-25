5d419f6a98b38b3b5850a48163e42813d0461051
Pasiexpress <pmjayasinghe1025@gmail.com>
2021-04-25 17:24:19 +0530

    add the mailService

---
+++ springboot-backend/src/main/java/com/example/springbootbackend/service/MailService.java
@@ -0,0 +1,33 @@
+package com.example.springbootbackend.service;
+import java.io.IOException;
+import java.io.IOException;
+
+public class MailService {
+	public void sendMail() throws IOException {
+	//	Email from = new Email("test@example.com");
+	//	Email to = new Email("pmjayasinghe1025@gmail.com"); // use your own email address here
+
+		String subject = "Sending with Twilio SendGrid is Fun";
+	//	Content content = new Content("text/html", "and <em>easy</em> to do anywhere with <strong>Java</strong>");
+
+	//	Mail mail = new Mail(from, subject, to, content);
+
+	//	SendGrid sg = new SendGrid(System.getenv("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"));
+	//	Request request = new Request();
+
+	//	request.setMethod(Method.POST);
+	//	request.setEndpoint("mail/send");
+	//	request.setBody(mail.build());
+
+	//	Response response = sg.api(request);
+
+	//	System.out.println(response.getStatusCode());
+	//	System.out.println(response.getHeaders());
+	//	System.out.println(response.getBody());
+
+
+	}
+
+
+
+}
