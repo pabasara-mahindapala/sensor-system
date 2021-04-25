215e43ab9673cf730f0dcb17a5cb21d573fb4db6
aba <you@example.com>
2021-04-25 22:10:13 +0530

    add mail alert to back-end

--- springboot-backend/src/main/java/com/example/springbootbackend/controller/SensorController.java
+++ springboot-backend/src/main/java/com/example/springbootbackend/controller/SensorController.java
@@ -3,16 +3,20 @@ package com.example.springbootbackend.controller;
 
 import com.example.springbootbackend.model.Sensor;
 import com.example.springbootbackend.repository.SensorRepository;
+import com.example.springbootbackend.service.MailService;
 import com.example.springbootbackend.service.SensorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.List;
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/v1/")
 public class SensorController {
-
+    @Autowired
+	private JavaMailSender javaMailSender;
     @Autowired
 	private SensorService sensorService;
 
@@ -24,11 +28,20 @@ public class SensorController {
 	// get all sensors
 	@GetMapping("/sensors")
 	public List<Sensor> getAllSensors(){
+		try {
+			new MailService(javaMailSender).sendMail();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+
 		return sensorService.findAll();
 	}
     // create sensor rest api
     @PostMapping("/sensors")
     public Sensor createSensor(@RequestBody Sensor sensor) {
-        return sensorService.createSensor(sensor);
+
+
+    	return sensorService.createSensor(sensor);
     }
 }
