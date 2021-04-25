965ee15130c757a255ad4c38b919f4293277f87a
pabasaramahindapala <pabasaramahindapala@gmail.com>
2021-04-25 19:15:41 +0530

    Construct chart with data

--- src/app/areas/charts/charts.component.ts
+++ src/app/areas/charts/charts.component.ts
@@ -6,7 +6,7 @@ declare const CanvasJS: any;
 
 interface SensorValue {
   x: Date;
-  y: number
+  y: number;
 }
 
 @Component({
@@ -26,55 +26,44 @@ export class ChartsComponent implements OnInit {
 
   ngOnInit(): void {
     this.sensorID = String(this.route.snapshot.paramMap.get('id'));
-    this.getSensors(this.sensorID);
-    var chart = new CanvasJS.Chart("chartContainer",
-      {
-        title: {
-          text: "Outdoor temperature"
-        },
-
-        axisX: {
-          title: "Time",
-          gridThickness: 2,
-          interval: 2,
-          intervalType: "hour",
-          valueFormatString: "YY.MM.DD hh:MM",
-          labelAngle: -20
-        },
-        axisY: {
-          title: "Reading"
-        },
-        data: [
-          {
-            type: "line",
-            dataPoints: [//array
-              { x: new Date(Date.UTC(2012, 1, 1, 1, 0)), y: 26 },
-              { x: new Date(Date.UTC(2012, 1, 1, 2, 0)), y: 38 },
-              { x: new Date(Date.UTC(2012, 1, 1, 3, 0)), y: 43 },
-              { x: new Date(Date.UTC(2012, 1, 1, 4, 0)), y: 29 },
-              { x: new Date(Date.UTC(2012, 1, 1, 5, 0)), y: 41 },
-              { x: new Date(Date.UTC(2012, 1, 1, 6, 0)), y: 54 },
-              { x: new Date(Date.UTC(2012, 1, 1, 7, 0)), y: 66 },
-              { x: new Date(Date.UTC(2012, 1, 1, 8, 0)), y: 60 },
-              { x: new Date(Date.UTC(2012, 1, 1, 9, 0)), y: 53 },
-              { x: new Date(Date.UTC(2012, 1, 1, 10, 0)), y: 60 }
-            ]
-          }
-        ]
-      });
-
-    chart.render();
+    this.fillChart(this.sensorID);
   }
 
-  getSensors(id: string) {
-    this.http.get("http://localhost:8080/api/v1/sensors" + id).subscribe(result => {
+  fillChart(id: string) {
+    this.http.get("http://localhost:8080/api/v1/sensors/" + id).subscribe(result => {
       this.sensorResponse = result;
       for (var s in this.sensorResponse) {
         this.sensorValues.push({
-          x: this.sensorResponse[s].date,
+          x: new Date(this.sensorResponse[s].date),
           y: this.sensorResponse[s].dataValue
         });
       }
+      var chart = new CanvasJS.Chart("chartContainer",
+        {
+          title: {
+            text: id
+          },
+
+          axisX: {
+            title: "Time",
+            gridThickness: 1,
+            interval: 10,
+            intervalType: "minute",
+            valueFormatString: "hh:MM",
+            labelAngle: -90
+          },
+          axisY: {
+            title: "Reading"
+          },
+          data: [
+            {
+              type: "line",
+              dataPoints: this.sensorValues
+            }
+          ]
+        });
+
+      chart.render();
     });
   }
 }
